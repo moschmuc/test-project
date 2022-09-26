411ffb556611e1251d4d47ef28020f56603359db
Momme Schmuck <schmuckm@telekom.de>
2022-09-26 13:58:42 +0200

    add dtos

--- src/client/doc_generator.go
+++
@@ -1,4 +0,0 @@
-package client
-
-//go:generate -command OAPI go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen -generate
-//go:generate OAPI types -o dtos.gen.go -package client internal/client/res/swagger.yml
