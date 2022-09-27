6bd54ddda59fe5b20bf91b032c81050d112ea0d1
Momme Schmuck <schmuckm@telekom.de>
2022-09-27 11:27:05 +0200

    change path doc.generator.go

--- src/dtos/doc_generator.go
+++ src/dtos/doc_generator.go
@@ -2,4 +2,4 @@ package dtos
 
 //go:generate -command OAPI go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen -generate
 
-//go:generate OAPI types --output-config --old-config-style --output-config -o dtos.gen.go -package dtos .dtos/res/swagger.yml
+//go:generate OAPI types --output-config --old-config-style --output-config -o dtos.gen.go -package dtos ./res/swagger.yml
