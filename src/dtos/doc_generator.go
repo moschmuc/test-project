4d705560ab397ec37eb403f0a9d82499df386fa6
Michael Krill <michael.krill@telekom.de>
2022-09-28 15:16:51 +0200

    feat: fix error generating the dtos.gen.go

--- src/dtos/doc_generator.go
+++ src/dtos/doc_generator.go
@@ -1,8 +1,10 @@
 package dtos
 
-//go:generate -command OAPI go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen -generate
+//go:generate -command OAPI go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen
 
 // Generate a dtos.gen.go and print the contents for a config yaml file to stdout, which could be used for
 // "OAPI --config <config.yaml> ./res/swagger.yml" later on instead of setting all command line parameters.
 // See https://github.com/deepmap/oapi-codegen/releases for details
-//go:generate OAPI types --output-config --old-config-style --output-config -o dtos.gen.go -package dtos ./res/swagger.yml
+// go:generate OAPI -generate types --old-config-style -o dtos.gen.go -package dtos ./res/swagger.yml
+
+//go:generate OAPI --config oapi_config.yaml ./res/swagger.yml
